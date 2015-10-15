e7efbd80e4901222ea7c287cedd994afd00cfcad
Micaiah-Chang <Micaiah@server.fake>
2015-10-14 21:01:56 -0700

    Create initial model for changesets

--- root/feature_list/models.py
+++ root/feature_list/models.py
@@ -1,3 +1,25 @@
-from django.db import models
+from django.db.models import (
+    Model,
+    CharField, DateTimeField, TextField, URLField,
+)
+
+# (date, description, lead developer, url, title)
 
 # Create your models here.
+class FeatureModel(Model):
+    # Indexing, as "latest date" and "Range of dates"
+    # are commonly used operations
+    date = DateTimeField("feature published", db_index=True)
+    description = TextField()
+    # Features do not necessarily have to be
+    # localized to one page, nor even be part of one
+    # Maybe needs to be updated to a Many to Many field later
+    url = URLField(null=True, blank=True)
+    # This may turn into a foreign key in the future, if
+    # feature searches are needed internally
+    lead_developer = CharField(max_length=200)
+    # Picking twitter length rules for no  good reason.
+    title = CharField(max_length=140)
+
+    def __unicode__(self):
+        return u"%s", self.title
