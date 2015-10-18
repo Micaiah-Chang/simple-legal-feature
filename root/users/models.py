b4e0e11b4a07dbab4d7c55b83f48275544247f0b
Micaiah-Chang <Micaiah@server.fake>
2015-10-17 18:42:47 -0700

    Fix functools wraps call

--- root/users/models.py
+++ root/users/models.py
@@ -14,7 +14,7 @@ def _user_decorator(create_user):
     """Force a user's 'last active' time
     to be always set when the user is registered.
     """
-    @wraps
+    @wraps(create_user)
     def register_last_active(*args, **kwargs):
         user = create_user(*args, **kwargs)
         active_time = UserActiveTime(user=user, last_active=datetime.now())
