3e53110ee61997f8dfb79c708acb2f088d38a813
Micaiah-Chang <Micaiah@server.fake>
2015-10-17 17:34:57 -0700

    Extend create_user func to register last_active

--- root/users/models.py
+++ root/users/models.py
@@ -3,4 +3,20 @@ from functools import wraps
 from django.db import models
 
 # Create your models here.
+class UserActiveTime(models.Model):
+    user = models.ForeignKey("models.User")
+    last_active = models.DateTimeField()
 
+def _user_decorator(create_user):
+    """Force a user's 'last active' time
+    to be always set when the user is registered.
+    """
+    @wraps
+    def register_last_active(*args, **kwargs):
+        user = create_user(*args, **kwargs)
+        active_time = UserActiveTime(user=user, last_active=datetime.now())
+        return user, active_time
+
+    return register_last_active
+
+create_user = _user_decorator(models.User.objects.create_user)
