8a5dbe52e91113fac011b4f237b2b82aaeaeaf82
Micaiah-Chang <Micaiah@server.fake>
2015-10-17 21:35:51 -0700

    Add small page for user login

--- root/root/urls.py
+++ root/root/urls.py
@@ -18,4 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^features/', include('feature_list.urls', namespace="features")),
+    url(r'^', include("users.urls", namespace="users")),
 ]
