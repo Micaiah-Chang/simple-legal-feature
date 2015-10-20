3efbd3c931d627af1da9f36ce03b164950ed9c6e
Micaiah-Chang <Micaiah@server.fake>
2015-10-20 11:43:21 -0700

    Add context processor to installed apps

---
+++ root/feature_list/context_processor.py
@@ -0,0 +1,18 @@
+from users.models import UserActiveTime
+from .models import FeatureModel
+import .services
+
+def check_newest(request):
+    user = request.user
+
+    if user.isauthenticated():
+        features = FeatureModel.objects.all()
+
+        last_seen = UserActiveTime.objects.get(user=user).first().last_active
+        is_new_feature = services.is_new_feature(features, last_seen)
+        return {"feature_changes": is_new_feature}
+    else:
+        # Just always have feature changes off
+        # as a guest user, or a preset number
+        # likely to increase conversion
+        return {"feature_changes": False}
