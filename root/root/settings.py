90a1dbbc95b0195d15f18f35941ddf56db76c214
Micaiah-Chang <Micaiah@server.fake>
2015-10-20 17:58:18 -0700

    Properly import context_processors

--- root/root/settings.py
+++ root/root/settings.py
@@ -12,9 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
@@ -64,7 +62,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'feature_list.context_processors.check_newest'
+                'feature_list.context_processors.check_newest',
             ],
             'loaders': [
                 'app_namespace.Loader',
