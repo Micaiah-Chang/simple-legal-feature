c3e8c18068e907f40eeab57f48336f7199c8efbc
Micaiah-Chang <Micaiah@server.fake>
2015-10-20 19:26:17 -0700

    Fix broken behavior of site when not user
    urls regexes between the root urls and the users urls now properly
    written so that they actually resolve to something when an anonymous
    user is browsing the site.

--- root/users/urls.py
+++ root/users/urls.py
@@ -2,16 +2,16 @@ from django.conf.urls import patterns, url
 from views import index
 urlpatterns = patterns('',
     url(
-        r'^login/$',
+        r'login/$',
         'django.contrib.auth.views.login',
         name='login',
         kwargs={'template_name': 'users/login.html'}
     ),
     url(
-        r'^logout/$',
+        r'logout/$',
         'django.contrib.auth.views.logout',
         name='logout',
         kwargs={'next_page': '/'}
     ),
-    url(r'^$', index, name="index"),
+    url(r'$', index, name="index"),
 )
